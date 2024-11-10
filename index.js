40126acd477337285b1a67e583921a8c1d625edb
Onela25 <onelawellem0525@gmail.com>
2024-11-10 09:43:37 +0200

    Basic html and Css and js

---
+++ index.js
@@ -0,0 +1,37 @@
+document.getElementById("darkModeToggle").addEventListener("click", function () {
+            document.body.classList.toggle("dark-mode");
+        });
+
+        
+        window.onscroll = function () {
+            const scrollTopBtn = document.getElementById("scrollTopBtn");
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                scrollTopBtn.style.display = "block";
+            } else {
+                scrollTopBtn.style.display = "none";
+            }
+        };
+
+        document.getElementById("scrollTopBtn").addEventListener("click", function () {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        });
+
+        
+        let currentIndex = 0;
+        const images = document.querySelectorAll("#carouselImages img");
+
+        function showImage(index) {
+            images.forEach((img, i) => {
+                img.style.display = i === index ? "block" : "none";
+            });
+        }
+
+        function startCarousel() {
+            showImage(currentIndex);
+            currentIndex = (currentIndex + 1) % images.length;
+            setTimeout(startCarousel, 3000);
+        }
+
+        startCarousel();
+    
